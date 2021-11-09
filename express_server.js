14bda223b169ef942e05490a2024a3cc62e8782d
amoham1 <amoham456@gmail.com>
2021-11-09 16:22:15 -0500

    continued to add some code and uderstand what -curl shows on a specific page

--- express_server.js
+++ express_server.js
@@ -6,13 +6,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+// on link localhost:8080 shows hello
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+// on link localhost:8080/urls.json shows object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+// on link localhost:8080/hello shows html style Hello World
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
